691ef167399f6e8eafc6beb82054265be3e2fbed
Kyytox <kytox.dev@protonmail.com>
2025-10-24 02:18:20 +0200

    upd tests

--- tests/data-explorer/parse_spec.lua
+++ tests/data-explorer/parse_spec.lua
@@ -1,36 +1,74 @@
 local plugin = require("data-explorer.core.parser")
 
-describe("Tests Parser module", function()
+describe("Data Explorer Parser", function()
 	-- Test for parse_csv
-	it("parse_csv", function()
+	it("parse_csv with delimiter ,", function()
 		local csv_text = [[
-      Header1,Header2,Count
-      Value1,Value2,20
-      Value3,Value4,20
+      Header1,Header2
+      Value1,Value2
+      Value3,Value4
     ]]
 
-		local result, err = plugin.parse_csv(csv_text)
+		local result, err = plugin.parse_csv(csv_text, ",")
 		assert.is_nil(err)
 		assert.are.same({ "Header1", "Header2" }, result.headers)
-		assert.are.same(
-			{ { Header1 = "Value1", Header2 = "Value2" }, { Header1 = "Value3", Header2 = "Value4" } },
-			result.data
-		)
-		assert.are.equal("20", result.count_lines)
+		assert.are.same({
+			{
+				Header1 = "      Value1",
+				Header2 = "Value2",
+			},
+			{
+				Header1 = "      Value3",
+				Header2 = "Value4",
+			},
+		}, result.data)
 	end)
 
-	-- -- Test for parse_columns_string
-	-- it("parse_columns_string", function()
-	-- 	local input = [[
-	--      Columns,nb_lines: "[{'name': 'Header1', 'type': 'STRING'}, {'name': 'Header2', 'type': 'STRING'}]",30]]
-	--
-	-- 	local result, err = plugin.parse_columns_string(input)
-	-- 	assert.is_nil(err)
-	-- 	assert.are.same({ "column", "type" }, result.headers)
-	-- 	assert.are.same({
-	-- 		{ column = "Header1", type = "STRING" },
-	-- 		{ column = "Header2", type = "STRING" },
-	-- 	}, result.data)
-	-- 	assert.are.equal("30", result.count_lines)
-	-- end)
+	-- Test for parse_csv with delimiter |
+	it("parse_csv with delimiter |", function()
+		local csv_text = [[
+      Header1|Header2
+      Value1|Value2
+      Value3|Value4
+    ]]
+
+		local result, err = plugin.parse_csv(csv_text, "|")
+		assert.is_nil(err)
+		assert.are.same({ "Header1", "Header2" }, result.headers)
+		assert.are.same({
+			{
+				Header1 = "      Value1",
+				Header2 = "Value2",
+			},
+			{
+				Header1 = "      Value3",
+				Header2 = "Value4",
+			},
+		}, result.data)
+	end)
+
+	-- Test for parse_raw_text
+	it("parse_raw_text", function()
+		local raw_text = [[
+┌────────────────┬─────────┬─────────┬
+│     Column     │  Type   │  Count  │
+│    varchar     │ varchar │  int64  │
+├────────────────┼─────────┼─────────┼
+│ nom            │ VARCHAR │     100 │
+│ prenom         │ VARCHAR │     100 │
+└────────────────┴─────────┴─────────┴]]
+
+		local result, err = plugin.parse_raw_text(raw_text)
+		assert.is_nil(err)
+		assert.are.same({
+			"┌────────────────┬─────────┬─────────┬",
+			"│     Column     │  Type   │  Count  │",
+			"│    varchar     │ varchar │  int64  │",
+			"├────────────────┼─────────┼─────────┼",
+			"│ nom            │ VARCHAR │     100 │",
+			"│ prenom         │ VARCHAR │     100 │",
+			"└────────────────┴─────────┴─────────┴",
+		}, result.data)
+		assert.are.equal(100, result.count_lines)
+	end)
 end)
