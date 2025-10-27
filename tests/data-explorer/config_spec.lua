b032320f8c504a06ac29aea2b18736b4fe5f86d0
Kyytox <kytox.dev@protonmail.com>
2025-10-27 22:08:25 +0100

    add test validation windows_opts

--- tests/data-explorer/config_spec.lua
+++ tests/data-explorer/config_spec.lua
@@ -1,36 +1,19 @@
 local config = require("data-explorer.gestion.config")
 
 describe("validate_options", function()
-	local defaults
-	before_each(function()
-		defaults = {
-			limit = 1000,
-			layout = "vertical",
-			window_opts = { border = "rounded" },
-			placeholder_sql = {
-				"SELECT * FROM f LIMIT 1000;",
-				"-- Warning: Large result could slow down / crash.",
-				"-- To query the file, use 'f' as the table name.",
-			},
-			files_types = {
-				parquet = true,
-				csv = true,
-				tsv = true,
-			},
-		}
-	end)
+	local defaults = config.defaults
 
 	it("reverts invalid limit", function()
 		local opts = { limit = -1 }
 		local msg = config.validate_options(opts)
-		assert.equals(config.defaults.limit, opts.limit)
+		assert.equals(defaults.limit, opts.limit)
 		assert.matches("limit must be a positive number", msg)
 	end)
 
 	it("reverts invalid layout", function()
 		local opts = { limit = 10, layout = "diagonal" }
 		local msg = config.validate_options(opts)
-		assert.equals(config.defaults.layout, opts.layout)
+		assert.equals(defaults.layout, opts.layout)
 		assert.matches("layout must be", msg)
 	end)
 
@@ -42,16 +25,16 @@ describe("validate_options", function()
 			files_types = defaults.files_types,
 		}
 		local msg = config.validate_options(opts)
-		assert.equals(config.defaults.placeholder_sql[1], defaults.placeholder_sql[1])
-		assert.equals(config.defaults.placeholder_sql[2], defaults.placeholder_sql[2])
-		assert.equals(config.defaults.placeholder_sql[3], defaults.placeholder_sql[3])
+		assert.equals(defaults.placeholder_sql[1], defaults.placeholder_sql[1])
+		assert.equals(defaults.placeholder_sql[2], defaults.placeholder_sql[2])
+		assert.equals(defaults.placeholder_sql[3], defaults.placeholder_sql[3])
 		assert.matches("placeholder_sql must be a table", msg)
 	end)
 
 	it("reverts invalid files_types types", function()
 		local opts = { limit = 10, layout = "vertical", files_types = "csv" }
 		local msg = config.validate_options(opts)
-		assert.equals(config.defaults.files_types[1], opts.files_types[1])
+		assert.equals(defaults.files_types[1], opts.files_types[1])
 		assert.matches("files_types must be a table", msg)
 	end)
 
@@ -61,9 +44,30 @@ describe("validate_options", function()
 			layout = "vertical",
 			files_types = { csv = true, xml = true },
 			placeholder_sql = defaults.placeholder_sql,
+			window_opts = defaults.window_opts,
 		}
 		local msg = config.validate_options(opts)
-		print(vim.inspect(msg))
 		assert.matches("Unsupported file type: xml", msg)
+	end)
+
+	it("reverts max_height_metadata, max_width_metadata to defaults if invalid", function()
+		local opts = {
+			limit = 10,
+			layout = "vertical",
+			window_opts = {
+				border = "rounded",
+				max_height_metadata = 1.5,
+				max_width_metadata = -0.2,
+			},
+			placeholder_sql = defaults.placeholder_sql,
+			files_types = defaults.files_types,
+		}
+		local msg = config.validate_options(opts)
+		assert.equals(defaults.window_opts.max_height_metadata, opts.window_opts.max_height_metadata)
+		assert.equals(defaults.window_opts.max_width_metadata, opts.window_opts.max_width_metadata)
+		assert.matches(
+			"max_height_metadata and max_width_metadata must be numbers between 0 and 1. Reverting to defaults.",
+			msg
+		)
 	end)
 end)
