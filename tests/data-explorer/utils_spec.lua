cb0175177459cecabc3d81d65e68c63d8ef398b5
Kyytox <kytox.dev@protonmail.com>
2025-10-30 02:15:35 +0100

    remove test find files

--- tests/data-explorer/utils_spec.lua
+++ tests/data-explorer/utils_spec.lua
@@ -12,19 +12,6 @@ describe("Tests Utils module", function()
 		assert.is_false(utils.is_accepted_file_type("image.png", accepted_types))
 	end)
 
-	-- Test for get_files_in_working_directory
-	it("get_files_in_working_directory", function()
-		local accepted_types = { "csv", "parquet" }
-		local files = (utils.get_files_in_working_directory(accepted_types))
-
-		-- Assuming there are some .lua and .md files in the working directory for this test
-		assert.is_true(#files > 0)
-
-		for _, file in ipairs(files) do
-			assert.is_true(utils.is_accepted_file_type(file, accepted_types))
-		end
-	end)
-
 	-- Test for aggregate files types
 	it("Test aggregate files types", function()
 		local opts = {
