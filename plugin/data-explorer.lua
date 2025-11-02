389e7b245a67739122273de63366bcc4cdc1bf22
Kyytox <kytox.dev@protonmail.com>
2025-11-02 20:35:06 +0100

    update file plugin for loading

--- plugin/data-explorer.lua
+++ plugin/data-explorer.lua
@@ -1,1 +1,14 @@
-require("data-explorer")
+-- require("data-explorer")
+
+if 1 ~= vim.fn.has("nvim-0.10.0") then
+	vim.api.nvim_err_writeln("data-explorer.nvim requires Neovim 0.10.0 or higher")
+	return
+end
+
+if vim.g.loaded_data_explorer == 1 then
+	return
+end
+vim.g.loaded_data_explorer = 1
+
+local data_explorer = require("data-explorer")
+data_explorer.setup()
